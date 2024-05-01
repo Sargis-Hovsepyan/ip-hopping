40a349c4ff599cd4723ae19e41c10bc4bf648989
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2024-05-01 17:04:15 +0400

    [FIX] Fixed the logic of receiving data and updating the address

--- lib/target.py
+++ lib/target.py
@@ -4,52 +4,64 @@ import ipaddress
 
 from hopping import ip_hopping
 
+
 class TargetServer:
     def __init__(self, initial_ip, port):
-        self.ip = initial_ip
+        self.host = initial_ip
         self.port = port
+        self.ip_hopped = initial_ip
         self.ip_range = ipaddress.ip_network('154.168.1.0/24')
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server.bind((self.ip, port))
-        self.server.listen(1)
-    
+
     def start(self):
-            seed = -1
-            while True:
-                seed = seed + 1
-                print(f"[TARGET SERVER LISTENNIG ON] : {self.ip}:{self.port}")
-                
-                client, address = self.server.accept()
-                with client:
-                    print(f"[NEW CONNECTION] : {address}")
+        self.server.bind((self.host, self.port))
+        self.server.listen(1)
 
-                    data = client.recv(1024)
-                    if not data:
-                        continue
+        seed = -1
+        while True:
+            seed = seed + 1
+            print(
+                f"[TARGET SERVER LISTENNIG ON] : {self.ip_hopped}:{self.port}")
 
-                    # Change IP address
-                    self.change_ip(data, address, seed)
-    
+            client, address = self.server.accept()
+            with client:
+                print(f"[NEW CONNECTION] : {address}")
+
+                data = client.recv(1024)
+                if not data:
+                    continue
+
+                # Change IP address
+                response, process = self.change_ip(data, address, seed)
+                if (process):
+                    self.process_data(response, address)
+                else:
+                    print(
+                        f"[TARGET SERVER]: Mismatch of IP Sent by Manager: Refusing to Process!")
+
     def change_ip(self, data, address, seed):
         client_data = data.decode('utf-8')
+        process = False
+
         last_line = client_data.split('\n')[-1]
-        index = last_line.find("NIP: ")
-        ip_address = last_line[index + len("NIP: "):].strip()
-        
-        new_ip = ip_hopping(self.ip_range, seed)
+        ip_address = last_line[len("NIP: "):].strip()
+
+        new_ip = self.host
+        if seed != 0:
+            new_ip = ip_hopping(self.ip_range, seed)
+
         hash_new_ip = hashlib.sha256(new_ip.encode()).hexdigest()
-        
-        #    self.server.close()
-        #    # Create a new socket with the new IP address
-        #    self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #    self.server.bind((new_ip, self.port))
-        #    self.server.listen(1)
-        
-        # if ip_address == hash_new_ip:
-        self.process_data(data, address)
-        self.ip = new_ip
-    
+        self.ip_hopped = new_ip
+
+        if ip_address == hash_new_ip:
+            process = True
+
+        print("ip_address: " + ip_address)
+        print("new_ip_address: " + hash_new_ip)
+
+        return client_data[:client_data.index("NIP:")].encode(), process
+
     def process_data(self, data, address):
         if data:
             print(f"[{address}]: " + data.decode('utf-8'))
@@ -57,10 +69,10 @@ class TargetServer:
             print(f"[{address}] : !EMPTY DATA")
 
 
-#--------------------------------------- #
+# --------------------------------------- #
 #                  MAIN                  #
-#--------------------------------------- #
+# --------------------------------------- #
 
 if __name__ == "__main__":
-    server = TargetServer('localhost', 5556)
+    server = TargetServer('127.0.0.1', 5556)
     server.start()
