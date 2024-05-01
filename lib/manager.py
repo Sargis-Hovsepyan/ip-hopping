76a4f05725c8bb82f09e0668ee65afba9b4c5668
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2024-05-01 17:03:23 +0400

    [FIX] Fixed the logic of connecting to the target

--- lib/manager.py
+++ lib/manager.py
@@ -5,6 +5,7 @@ import ipaddress
 
 from hopping import ip_hopping
 
+
 class IPHopperManager:
     def __init__(self, host, port, target_initial_ip, target_port):
         self.host = host
@@ -17,7 +18,7 @@ class IPHopperManager:
         self.clients = {}
 
         self.ip_range = ipaddress.ip_network('154.168.1.0/24')
-    
+
     def start(self):
         self.server.bind((self.host, self.port))
         self.server.listen()
@@ -26,68 +27,66 @@ class IPHopperManager:
 
         while True:
             client, address = self.server.accept()
-            
-            thread = threading.Thread(target=self.handle_clients, args=(client, address))
+
+            thread = threading.Thread(
+                target=self.handle_clients, args=(client, address))
             thread.start()
-            
+
             print(f'[ACTIVE CONNECTIONS] : {threading.active_count() - 1}')
-    
+
     def handle_clients(self, client, address):
         print(f"[NEW CONNECTION] : {address}")
-        
+
         seed = -1
         while True:
             seed = seed + 1
             data = client.recv(1024)
-            
+
             if data.decode('utf-8') == '!DISCONNECT':
                 print(f'[{address} : Disconnected.')
                 break
 
-            # Generate IP for target
-            # if (seed != 0):
-            #     self.tareget_ip = ip_hopping(self.ip_range, seed=seed)
-            
             response = self.process_data(data, address, seed)
+            self.send_to_target(response, self.target_ip, self.target_port)
 
-            # Create a new Connection
-            client.close()
-            
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect((self.target_ip, self.target_port))
-            client.sendall(response)
-        
         # Closing the Connection.
         client.close()
-    
+
+    def send_to_target(self, message, ip, port):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((ip, port))
+        client.sendall(message)
+
+        client.close()
+
     def process_data(self, data, address, seed):
         # Process received data and return response
-        if data:
-            new_ip = self.target_ip
-            if seed != 0:
-                new_ip = ip_hopping(self.ip_range, seed)
-            
-            print(f"[{address}]: " + data.decode('utf-8'))
-            
-            # Decode the received bytes into a string
-            client_data = data.decode()
-            
-            # Hash the new IP address
-            hashed_ip = hashlib.sha256(new_ip.encode()).hexdigest()
-            
-            # Add hashed IP to the client data
-            response = client_data + "\nNIP: " + hashed_ip
-            
-            # Encode the processed data as bytes
-            return response.encode()
-        else:
+        if not data:
             print(f"[{address}] : !EMPTY DATA")
             return data
 
+        new_ip = self.target_ip
+        if seed != 0:
+            new_ip = ip_hopping(self.ip_range, seed)
 
-#--------------------------------------- #
+        print(f"[{address}]: " + data.decode('utf-8'))
+
+        # Decode the received bytes into a string
+        client_data = data.decode()
+
+        # Hash the new IP address
+        hashed_ip = hashlib.sha256(new_ip.encode()).hexdigest()
+
+        # Add hashed IP to the client data
+        response = client_data + "\nNIP: " + hashed_ip
+
+        # Encode the processed data as bytes
+        return response.encode()
+
+
+# --------------------------------------- #
 #                  MAIN                  #
-#--------------------------------------- #
+# --------------------------------------- #
 
 if __name__ == "__main__":
     server = IPHopperManager('127.0.0.1', 5555, '127.0.0.1', 5556)
