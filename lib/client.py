270dc142b2314870bd747fdd3b5eaef133b5d6e1
Sargis-Hovsepyan <sargishovsepyan2001@gmail.com>
2024-05-01 17:03:00 +0400

    [UPDATE] Changesd custom messages

--- lib/client.py
+++ lib/client.py
@@ -1,27 +1,27 @@
 import socket
 
+
 class Client:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        
+
         self.client = None
 
     def close_connection(self):
         if self.client:
             self.client.close()
-    
+
     def connect(self):
         try:
             self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.client.connect((self.host, self.port))
             self.connected = True
-            print("Connected to server.")
+            print("Connected to server!!!\n")
         except ConnectionRefusedError:
             print("Connection refused. Server may be down.")
         except Exception as e:
-            print(f"Error occurred during connection: {e}") 
-   
+            print(f"Error occurred during connection: {e}")
 
     def send_message(self, message):
         if not self.connected:
@@ -30,7 +30,7 @@ class Client:
 
         try:
             self.client.sendall(message.encode('utf-8'))
-            print("Message sent successfully.")
+            print("Message sent successfully!\n")
         except ConnectionAbortedError:
             print("Connection aborted. Reconnecting...")
             self.connect()
@@ -39,9 +39,9 @@ class Client:
             print(f"Error occurred while sending message: {e}")
 
 
-#--------------------------------------- #
+# --------------------------------------- #
 #                  MAIN                  #
-#--------------------------------------- #
+# --------------------------------------- #
 
 if __name__ == "__main__":
     host = "localhost"
@@ -56,5 +56,5 @@ if __name__ == "__main__":
 
         if message == '!DISCONNECT':
             break
-    
+
     client.close_connection()
